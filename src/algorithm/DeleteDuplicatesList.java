c51e22739d5a84831d9c647c5cf470e5ab284c6d
wangty9153@126.com <wangty9153@126.com>
2019-12-11 11:18:58 +0800

    删除链表重复元素

--- src/algorithm/DeleteDuplicatesList.java
+++ src/algorithm/DeleteDuplicatesList.java
@@ -2,6 +2,14 @@ package algorithm;
 
 import data.ListNode;
 
+/**
+ * created by Sven
+ * on 2019
+ *
+ * 删除排序链表中的重复元素
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
 public class DeleteDuplicatesList {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null)
