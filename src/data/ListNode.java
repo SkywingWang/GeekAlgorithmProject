5036981ec7e7bf1d20979645d03715206a5f9676
wangty9153@126.com <wangty9153@126.com>
2019-12-12 11:31:35 +0800

    查找相同的树

--- src/data/ListNode.java
+++ src/data/ListNode.java
@@ -1,5 +1,11 @@
 package data;
 
+/**
+ * created by Sven
+ * on 2019-12-03
+ *
+ * List数据结构
+ */
 public class ListNode {
     public int val;
     public ListNode next;
---
+++ src/data/TreeNode.java
@@ -0,0 +1,14 @@
+package data;
+
+/**
+ * created by Sven
+ * on 2019-12-03
+ *
+ * Tree数据结构
+ */
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int x) { val = x; }
+}
