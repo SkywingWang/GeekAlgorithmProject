474f632c06990f677e3091971579ed21bcef47bd
wangty9153@126.com <wangty9153@126.com>
2019-12-13 11:23:29 +0800

    对称二叉树

---
+++ src/algorithm/SymmetricTree.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import data.TreeNode;
+
+/**
+ * created by Sven
+ * on 2019-12-12
+ *
+ * 镜像树 对称二叉树
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ */
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root){
+        return isMirror(root,root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val)
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
+    }
+}
