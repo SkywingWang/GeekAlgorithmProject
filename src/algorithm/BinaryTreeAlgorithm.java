c39bf303c2a7edca74627760e5531bfb5f990ed8
wangty9153@126.com <wangty9153@126.com>
2019-12-21 11:58:20 +0800

    二叉树路径

--- src/algorithm/BinaryTreeAlgorithm.java
+++ src/algorithm/BinaryTreeAlgorithm.java
@@ -222,4 +222,25 @@ public class BinaryTreeAlgorithm {
         return min_depth;
 
     }
+
+    /**
+     * created by Sven
+     * on 2019-12-20
+     *
+     * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+     *
+     * 说明: 叶子节点是指没有子节点的节点。
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+        if(root.left == null && root.right == null){
+            if(root.val != sum)
+                return false;
+            else
+                return true;
+        }
+        return hasPathSum(root.left,sum - root.val) || hasPathSum(root.right,sum-root.val);
+
+    }
 }
