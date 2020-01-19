16e8825506c7a69051bcccb9b78db0ef5afbfdf5
skywingking <wangty9153@126.com>
2020-01-19 14:23:03 +0800

    买卖股票的最佳时机

--- src/algorithm/VariousAlgorithm.java
+++ src/algorithm/VariousAlgorithm.java
@@ -97,4 +97,31 @@ public class VariousAlgorithm {
         product *= n;
         return product - count;
     }
+
+    /**
+     * 买卖股票的最佳时机
+     *
+     * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+     *
+     * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+     *
+     * 注意你不能在买入股票前卖出股票。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int spreadPrice = 0;
+        if(prices == null || prices.length == 0)
+            return 0;
+        for(int i = 0; i < prices.length;i++){
+            if(prices[i] < minPrice)
+                minPrice = prices[i];
+            else if(prices[i] - minPrice > spreadPrice){
+                spreadPrice = prices[i] - minPrice;
+            }
+        }
+        return spreadPrice;
+    }
 }
