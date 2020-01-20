cae069d8cc907ff92e2b8a529b9de4a7e1b9dbd3
skywingking <wangty9153@126.com>
2020-01-20 15:18:07 +0800

    验证回文串

--- src/algorithm/StringAlgorithm.java
+++ src/algorithm/StringAlgorithm.java
@@ -124,4 +124,35 @@ public class StringAlgorithm {
         return result.toString();
 
     }
+
+    /**
+     * 验证回文串
+     *
+     * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+     *
+     * 说明：本题中，我们将空字符串定义为有效的回文串。
+     *
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+        if(s == null || "".equals(s))
+            return true;
+        int l = 0, r = s.length() - 1;
+        s = s.toUpperCase();
+        while(l < r){
+            if(!Character.isLetterOrDigit(s.charAt(l)))
+                l ++;
+            else if(!Character.isLetterOrDigit(s.charAt(r)))
+                r --;
+            else if(s.charAt(l) == s.charAt(r)){
+                l++;
+                r--;
+            }else{
+                return false;
+            }
+
+        }
+        return true;
+    }
 }
