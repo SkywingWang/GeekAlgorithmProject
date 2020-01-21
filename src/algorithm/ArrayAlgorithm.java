e5dcb963756218384ce091e0c32e62c99edf9be2
skywingking <wangty9153@126.com>
2020-01-21 18:08:31 +0800

    验证环形链表

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -339,4 +339,26 @@ public class ArrayAlgorithm {
         }
         return count;
     }
+
+    /**
+     * 只出现一次的数字
+     *
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     *
+     * 说明：
+     *
+     * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+     *
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int result = 0;
+        for(int i : nums){
+            result ^= i;
+        }
+        return result;
+    }
 }
--- src/algorithm/ListAlgorithm.java
+++ src/algorithm/ListAlgorithm.java
@@ -137,4 +137,26 @@ public class ListAlgorithm {
         }
         return result;
     }
+
+    /**
+     * 环形链表
+     *
+     * 给定一个链表，判断链表中是否有环。
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast){
+            if(fast.next == null || fast.next.next == null)
+                return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
 }
