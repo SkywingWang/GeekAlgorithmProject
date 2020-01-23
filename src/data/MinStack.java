f3015636c2269c8adda4bc2e70758aa516b591ca
skywingking <wangty9153@126.com>
2020-01-23 13:59:09 +0800

    最小栈

--- src/data/MinStack.java
+++ src/data/MinStack.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.Stack;
+
 /**
  * 最小栈
  *
@@ -13,23 +15,45 @@ package data;
  */
 public class MinStack {
     /** initialize your data structure here. */
-    public MinStack() {
 
+    // 数据栈
+    private Stack<Integer> data;
+
+    // 辅助栈
+    private Stack<Integer> helper;
+
+    public MinStack() {
+        // 数据栈
+        data = new Stack<>();
+        // 辅助栈
+        helper = new Stack<>();
     }
 
     public void push(int x) {
-
+        data.add(x);
+        if(helper.isEmpty() || helper.peek() >= x){
+            helper.add(x);
+        }else{
+            helper.add(helper.peek());
+        }
     }
 
     public void pop() {
-
+        if(!data.isEmpty()){
+            helper.pop();
+            data.pop();
+        }
     }
 
     public int top() {
-
+        if(!data.isEmpty()){
+            return data.peek();
+        }throw new RuntimeException("栈中元素为空，此操作非法");
     }
 
     public int getMin() {
-
+        if(!helper.isEmpty()){
+            return helper.peek();
+        }throw new RuntimeException("栈中元素为空，此操作非法");
     }
 }
