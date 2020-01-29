641617d84fcb57a5099e6cd80d9f719f382be3fb
skywingking <wangty9153@126.com>
2020-01-30 00:35:59 +0800

    两数之和

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -1,7 +1,6 @@
 package algorithm;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * created by Sven
@@ -360,5 +359,37 @@ public class ArrayAlgorithm {
             result ^= i;
         }
         return result;
+    }
+
+    /**
+     * 两数之和 II - 输入有序数组
+     *
+     * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+     *
+     * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+     *
+     * 说明:
+     *
+     * 返回的下标值（index1 和 index2）不是从零开始的。
+     * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+     *
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum_167(int[] numbers, int target) {
+        if(numbers == null || numbers.length < 2)
+            return null;
+        int left = 0,right = numbers.length - 1;
+        while (left < right){
+            if(numbers[left] + numbers[right] == target)
+                return new int[]{left + 1,right + 1};
+            else if(numbers[left] + numbers[right] > target)
+                right--;
+            else
+                left ++;
+        }
+        return null;
     }
 }
