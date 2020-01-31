4117ca615153db3c58443ad3aaedeb206826748a
skywingking <wangty9153@126.com>
2020-01-31 21:04:35 +0800

    多数元素

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -392,4 +392,22 @@ public class ArrayAlgorithm {
         }
         return null;
     }
+
+    /**
+     * 多数元素
+     *
+     * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+     *
+     * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
 }
