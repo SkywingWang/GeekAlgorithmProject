ec8de44316d8d24b7fd0ad113a7a6bbebe639a94
skywingking <wangty9153@126.com>
2020-02-03 00:09:35 +0800

    位1的个数

--- src/algorithm/VariousAlgorithm.java
+++ src/algorithm/VariousAlgorithm.java
@@ -233,4 +233,44 @@ public class VariousAlgorithm {
 
         return count;
     }
+
+    /**
+     * 颠倒二进制位
+     *
+     * 颠倒给定的 32 位无符号整数的二进制位。
+     *
+     * @param n
+     * @return
+     */
+    public int reverseBits(int n) {
+        int res = 0;
+        int count = 0;
+        while (count < 32){
+            res <<= 1;
+            res |= (n & 1);
+            n >>= 1;
+            count ++;
+        }
+        return res;
+    }
+
+    /**
+     * 位1的个数
+     *
+     * 编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+     *
+     * @param n
+     * @return
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+        int mask = 1;
+        for(int i = 0; i < 32; i ++){
+            if((n & mask) != 0){
+                count ++;
+            }
+            mask <<= 1;
+        }
+        return count;
+    }
 }
