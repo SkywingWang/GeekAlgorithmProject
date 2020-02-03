f9c0b909fa2cc8e7185241c712d551eecd13f864
skywingking <wangty9153@126.com>
2020-02-04 00:33:04 +0800

    快乐数

--- src/algorithm/VariousAlgorithm.java
+++ src/algorithm/VariousAlgorithm.java
@@ -273,4 +273,35 @@ public class VariousAlgorithm {
         }
         return count;
     }
+
+    /**
+     * 快乐数
+     *
+     * 编写一个算法来判断一个数是不是“快乐数”。
+     *
+     * 一个“快乐数”定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是无限循环但始终变不到 1。如果可以变为 1，那么这个数就是快乐数。
+     *
+     *
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n) {
+        int slow = n,fast = n;
+        do{
+            slow = bitSquareSum(slow);
+            fast = bitSquareSum(fast);
+            fast = bitSquareSum(fast);
+        }while (slow != fast);
+        return slow == 1;
+    }
+
+    private int bitSquareSum(int n){
+        int sum = 0;
+        while (n > 0){
+            int square = n % 10;
+            sum += (square * square);
+            n /= 10;
+        }
+        return sum;
+    }
 }
