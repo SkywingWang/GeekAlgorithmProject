8216f98b8bd281e58a385e5fe5e8fda9f42acc16
Skywing <wangty9153@126.com>
2020-03-24 11:26:41 +0800

    按摩师，动归

--- src/algorithm/VariousAlgorithm.java
+++ src/algorithm/VariousAlgorithm.java
@@ -445,4 +445,32 @@ public class VariousAlgorithm {
         return nextStates;
     }
 
+    /**
+     * 面试题 17.16. 按摩师
+     *
+     * 一个有名的按摩师会收到源源不断的预约请求，每个预约都可以选择接或不接。
+     * 在每次预约服务之间要有休息时间，因此她不能接受相邻的预约。给定一个预约请求序列，
+     * 替按摩师找到最优的预约集合（总预约时间最长），返回总的分钟数。
+     *
+     * 动态规划
+     *
+     * @param nums
+     * @return
+     */
+    public int massage(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        if(nums.length == 1)
+            return nums[0];
+
+        if(nums.length == 2)
+            return Math.max(nums[0],nums[1]);
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        result[1] = Math.max(nums[0],nums[1]);
+        for(int i = 2; i < nums.length;i++){
+            result[i] = Math.max(result[i - 1],result[i - 2] + nums[i]);
+        }
+        return result[nums.length - 1];
+    }
 }
