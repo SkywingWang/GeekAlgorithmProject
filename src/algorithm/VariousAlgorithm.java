b733400d1531d6717d3c3e5d6700991f3840f577
Skywing <wangty9153@126.com>
2020-03-28 17:19:52 +0800

    压缩字符串

--- src/algorithm/VariousAlgorithm.java
+++ src/algorithm/VariousAlgorithm.java
@@ -589,4 +589,15 @@ public class VariousAlgorithm {
         return result;
     }
 
+    /**
+     * 1342. 将数字变成 0 的操作次数
+     *
+     * 给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+     *
+     * @param num
+     * @return
+     */
+    public int numberOfSteps (int num) {
+        return 0;
+    }
 }
