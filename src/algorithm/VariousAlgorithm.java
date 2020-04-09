fdf1a3eb0cbc268b76a1b0c4ed1b25aad4490b76
Skywing <wangty9153@126.com>
2020-04-09 08:45:37 +0800

    22. 括号生成

--- src/algorithm/VariousAlgorithm.java
+++ src/algorithm/VariousAlgorithm.java
@@ -685,4 +685,42 @@ public class VariousAlgorithm {
         return res;
 
     }
+
+    /**
+     * 22. 括号生成
+     * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+     *
+     * 例如，给出 n = 3，生成结果为：
+     * @param n
+     * [
+     *   "((()))",
+     *   "(()())",
+     *   "(())()",
+     *   "()(())",
+     *   "()()()"
+     * ]
+     * @return
+     */
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result,new StringBuilder(),0,0,n);
+        return result;
+    }
+
+    private void backtrack(List<String> result,StringBuilder cur,int open,int close,int max){
+        if(cur.length() == max * 2){
+            result.add(cur.toString());
+            return;
+        }
+        if(open<max){
+            cur.append('(');
+            backtrack(result,cur,open + 1,close,max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if(close < open){
+            cur.append(')');
+            backtrack(result,cur,open,close+1,max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
 }
