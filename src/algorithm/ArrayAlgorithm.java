4d861e5d5611f03f7eadbf6e7a27dc6ef26840ea
Skywing <wangty9153@126.com>
2020-04-17 18:37:25 +0800

    55 跳跃游戏

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -878,4 +878,28 @@ public class ArrayAlgorithm {
         }
         return res;
     }
+
+    /**
+     * 55. 跳跃游戏
+     * 给定一个非负整数数组，你最初位于数组的第一个位置。
+     *
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     *
+     * 判断你是否能够到达最后一个位置。
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canJump(int[] nums) {
+        int rightMost = 0;
+        for(int i = 0; i < nums.length; i ++){
+            if(i<= rightMost){
+                rightMost = Math.max(rightMost, i + nums[i]);
+                if(rightMost >= nums.length - 1)
+                    return true;
+            }else
+                return false;
+        }
+        return false;
+    }
 }
