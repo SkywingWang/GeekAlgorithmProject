160b9a3a87a681c886f6221d0369bcc7595f6ddb
Skywing <wangty9153@126.com>
2020-04-29 13:28:21 +0800

    1095. 山脉数组中查找目标值

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import data.MountainArray;
+
 import java.util.*;
 
 /**
@@ -1073,6 +1075,109 @@ public class ArrayAlgorithm {
                 else
                     right = mid - 1;
             }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 面试题56 - I. 数组中数字出现的次数
+     *
+     * 一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+     *
+     * @param nums
+     * @return
+     */
+    public int[] singleNumbers(int[] nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum ^= nums[i];
+        }
+        int first = 1;
+        while ((sum & first) == 0)
+            first = first << 1;
+        int[] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            //将数组分类。
+            if((nums[i]&first)==0){
+                result[0]^=nums[i];
+            }
+            else{
+                result[1]^=nums[i];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 1095. 山脉数组中查找目标值
+     * （这是一个 交互式问题 ）
+     *
+     * 给你一个 山脉数组 mountainArr，请你返回能够使得 mountainArr.get(index) 等于 target 最小 的下标 index 值。
+     *
+     * 如果不存在这样的下标 index，就请返回 -1。
+     *
+     *  
+     *
+     * 何为山脉数组？如果数组 A 是一个山脉数组的话，那它满足如下条件：
+     *
+     * 首先，A.length >= 3
+     *
+     * 其次，在 0 < i < A.length - 1 条件下，存在 i 使得：
+     *
+     * A[0] < A[1] < ... A[i-1] < A[i]
+     * A[i] > A[i+1] > ... > A[A.length - 1]
+     *  
+     *
+     * 你将 不能直接访问该山脉数组，必须通过 MountainArray 接口来获取数据：
+     *
+     * MountainArray.get(k) - 会返回数组中索引为k 的元素（下标从 0 开始）
+     * MountainArray.length() - 会返回该数组的长度
+     *  
+     *
+     * 注意：
+     *
+     * 对 MountainArray.get 发起超过 100 次调用的提交将被视为错误答案。此外，任何试图规避判题系统的解决方案都将会导致比赛资格被取消。
+     *
+     * 为了帮助大家更好地理解交互式问题，我们准备了一个样例 “答案”：https://leetcode-cn.com/playground/RKhe3ave，请注意这 不是一个正确答案。
+     *
+     * @param target
+     * @param mountainArr
+     * @return
+     */
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        if(mountainArr == null || mountainArr.length() == 0)
+            return -1;
+        int left = 0, right = mountainArr.length() - 1;
+        int mid =  left + ((right - left) >> 1);
+        while (left < right){
+            if(mountainArr.get(mid) < mountainArr.get(mid + 1))
+                left = mid + 1;
+            else
+                right = mid;
+            mid = left + ((right - left) >> 1);
+        }
+        left = 0;
+        int tmp = right;
+        int midLeft = (left + right) / 2;
+        while (left < right){
+            if(mountainArr.get(midLeft) == target) return midLeft;
+            if(mountainArr.get(midLeft) > target)
+                right = midLeft;
+            else
+                left = midLeft + 1;
+            midLeft = (left + right) / 2;
+        }
+        left = tmp;
+        right = mountainArr.length();
+        int midRight = (left + right) / 2;
+        while (left < right){
+            if(mountainArr.get(midRight) == target) return midRight;
+            if(mountainArr.get(midRight) > target)
+                left = midRight + 1;
+            else
+                right = midRight;
+            midRight = (left + right) /2;
         }
         return -1;
     }
---
+++ src/data/MountainArray.java
@@ -0,0 +1,10 @@
+package data;
+
+/**
+ * Created by skywingking
+ * on 2020/4/29
+ */
+public interface MountainArray {
+    public int get(int index);
+    public int length();
+}
