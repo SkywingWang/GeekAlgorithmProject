5c9685c1afef63ab3371198985118e43ee40c40b
Skywing <wangty9153@126.com>
2020-05-01 12:16:56 +0800

    面试题 02.03. 删除中间节点

--- src/algorithm/ListAlgorithm.java
+++ src/algorithm/ListAlgorithm.java
@@ -341,4 +341,22 @@ public class ListAlgorithm {
 
 
     }
+
+    /**
+     * 面试题 02.03. 删除中间节点
+     *
+     * 实现一种算法，删除单向链表中间的某个节点（除了第一个和最后一个节点，不一定是中间节点），假定你只能访问该节点。
+     *
+     * @param node
+     */
+    public void deleteNode(ListNode node) {
+        if(node  == null)
+            return;
+        if(node.next == null){
+            node = null;
+            return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
--- src/algorithm/StringAlgorithm.java
+++ src/algorithm/StringAlgorithm.java
@@ -355,4 +355,20 @@ public class StringAlgorithm {
         }
         return countS2/n2;
     }
+
+    /**
+     * 面试题58 - II. 左旋转字符串
+     * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+     *
+     * @param s
+     * @param n
+     * @return
+     */
+    public String reverseLeftWords(String s, int n) {
+        if(s == null)
+            return null;
+        if(n > s.length())
+            return s;
+        return s.substring(n) + s.substring(0,n);
+    }
 }
