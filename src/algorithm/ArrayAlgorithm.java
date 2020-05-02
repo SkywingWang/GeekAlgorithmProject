a02db07c0f25994d623ac5fe28afea7cb56ba6cb
Skywing <wangty9153@126.com>
2020-05-02 12:55:13 +0800

    3. 无重复字符的最长子串

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -1211,4 +1211,60 @@ public class ArrayAlgorithm {
         }
         return resultArray;
     }
+
+    /**
+     * 1365. 有多少小于当前数字的数字
+     *
+     * 给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
+     *
+     * 换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
+     *
+     * 以数组形式返回答案。
+     *
+     * @param nums
+     * @return
+     */
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return new int[0];
+        int[] temp = new int[101];
+        for(int i = 0; i < nums.length;i++){
+            temp[nums[i]] += 1;
+        }
+        for(int i = 1; i < temp.length; i++){
+            temp[i] = temp[i - 1] + temp[i];
+        }
+        int[] result = new int[nums.length];
+        for(int i = 0; i < result.length; i ++){
+            if(nums[i]==0)
+                result[i] = 0;
+            else{
+                result[i] = temp[nums[i] - 1];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 3. 无重复字符的最长子串
+     * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        int rk = -1,ans = 0;
+        for(int i = 0; i < n; ++i){
+            if(i!=0)
+                occ.remove(s.charAt(i - 1));
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))){
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            ans = Math.max(ans,rk - i + 1);
+        }
+        return ans;
+
+    }
 }
