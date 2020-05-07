88612fbcefefb6670b9e7430cb776a1d05a29e5f
Skywing <wangty9153@126.com>
2020-05-07 13:15:47 +0800

    572. 另一个树的子树

--- src/algorithm/BinaryTreeAlgorithm.java
+++ src/algorithm/BinaryTreeAlgorithm.java
@@ -303,4 +303,25 @@ public class BinaryTreeAlgorithm {
         return true;
 
     }
+
+    /**
+     * 572. 另一个树的子树
+     * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(t==null) return true;
+        if(s==null) return false;
+        return isSubtree(s.left,t) || isSubtree(s.right,t) || isSameTreeSub(s,t);
+    }
+
+    private boolean isSameTreeSub(TreeNode s,TreeNode t){
+        if(s == null && t == null) return true;
+        if(s == null || t == null) return false;
+        if(s.val != t.val) return false;
+        return isSameTreeSub(s.left,t.left) && isSameTreeSub(s.right,t.right);
+    }
 }
--- src/algorithm/VariousAlgorithm.java
+++ src/algorithm/VariousAlgorithm.java
@@ -940,4 +940,48 @@ public class VariousAlgorithm {
         }
         return strBuilder.toString();
     }
+
+    /**
+     * 983. 最低票价
+     * 在一个火车旅行很受欢迎的国度，你提前一年计划了一些火车旅行。在接下来的一年里，你要旅行的日子将以一个名为 days 的数组给出。每一项是一个从 1 到 365 的整数。
+     *
+     * 火车票有三种不同的销售方式：
+     *
+     * 一张为期一天的通行证售价为 costs[0] 美元；
+     * 一张为期七天的通行证售价为 costs[1] 美元；
+     * 一张为期三十天的通行证售价为 costs[2] 美元。
+     * 通行证允许数天无限制的旅行。 例如，如果我们在第 2 天获得一张为期 7 天的通行证，那么我们可以连着旅行 7 天：第 2 天、第 3 天、第 4 天、第 5 天、第 6 天、第 7 天和第 8 天。
+     *
+     * 返回你想要完成在给定的列表 days 中列出的每一天的旅行所需要的最低消费。
+     *
+     * @param days
+     * @param costs
+     * @return
+     */
+
+    int[] costs;
+    Integer[] memo;
+    Set<Integer> dayset;
+    public int mincostTickets(int[] days, int[] costs) {
+        this.costs = costs;
+        memo = new Integer[366];
+        dayset = new HashSet<>();
+        for(int d : days){
+            dayset.add(d);
+        }
+        return mincostTicketsDP(1);
+    }
+
+    private int mincostTicketsDP(int i){
+        if(i > 365)
+            return 0;
+        if(memo[i] != null)
+            return memo[i];
+        if(dayset.contains(i)){
+            memo[i] = Math.min(Math.min(mincostTicketsDP(i + 1) + costs[0],mincostTicketsDP(i + 7) + costs[1]),mincostTicketsDP(i + 30) + costs[2]);
+        }else{
+            memo[i] = mincostTicketsDP(i + 1);
+        }
+        return memo[i];
+    }
 }
