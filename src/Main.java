f5eb64ff72005bba7f0f80b53f27b6ce28ab915f
Skywing <wangty9153@126.com>
2020-05-19 14:04:59 +0800

    验证回文字符串 Ⅱ

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,10 @@ import algorithm.VariousAlgorithm;
 public class Main {
 
     public static void main(String[] args) {
-        VariousAlgorithm variousAlgorithm = new VariousAlgorithm();
-        for(int i = 0; i < 7; i++){
-            System.out.println(variousAlgorithm.randomStr(7));
-        }
-        ArrayAlgorithm arrayAlgorithm = new ArrayAlgorithm();
-        int[] coins = {2,3,10};
-        System.out.println("result : " + arrayAlgorithm.minCount(coins));
+        int i = 2001;
+        int j = 1;
+        System.out.println("result : " + ((i & j) == j));
+
+
     }
 }
--- src/algorithm/StringAlgorithm.java
+++ src/algorithm/StringAlgorithm.java
@@ -371,4 +371,44 @@ public class StringAlgorithm {
             return s;
         return s.substring(n) + s.substring(0,n);
     }
+
+    /**
+     * 680. 验证回文字符串 Ⅱ
+     *
+     * 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+     * @param s
+     * @return
+     */
+    public boolean validPalindrome(String s) {
+        if(s == null || s.length() == 0)
+            return false;
+        if(s.length() <= 2)
+            return true;
+        int left = 0,right = s.length() - 1;
+        while (left < right){
+            char c1 = s.charAt(left), c2 = s.charAt(right);
+            if(c1 == c2){
+                left++;
+                right--;
+            }else{
+                boolean flag1 = true,flag2 = true;
+                for(int i = left,j = right - 1;i < j; i++,j--){
+                    char c3 = s.charAt(i),c4 = s.charAt(j);
+                    if(c3 != c4){
+                        flag1 = false;
+                        break;
+                    }
+                }
+                for(int i = left + 1,j = right;i<j;i++,j--){
+                    char c3 = s.charAt(i),c4 = s.charAt(j);
+                    if(c3 != c4){
+                        flag2 = false;
+                        break;
+                    }
+                }
+                return flag1 || flag2;
+            }
+        }
+        return true;
+    }
 }
