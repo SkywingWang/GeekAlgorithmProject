00db745fb484ceae711bff70536470768d222fd5
Skywing <wangty9153@126.com>
2020-05-22 15:38:19 +0800

    105. 从前序与中序遍历序列构造二叉树

--- src/algorithm/BinaryTreeAlgorithm.java
+++ src/algorithm/BinaryTreeAlgorithm.java
@@ -381,4 +381,41 @@ public class BinaryTreeAlgorithm {
         }
         return result;
     }
+
+    /**
+     * 105. 从前序与中序遍历序列构造二叉树
+     *
+     * 根据一棵树的前序遍历与中序遍历构造二叉树。
+     *
+     * 注意:
+     * 你可以假设树中没有重复的元素。
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder == null || preorder.length == 0)
+            return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for(int i = 1; i < preorder.length; i++){
+            int preOrderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if(node.val != inorder[inorderIndex]){
+                node.left = new TreeNode(preOrderVal);
+                stack.push(node.left);
+            }else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]){
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preOrderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
 }
--- src/algorithm/StringAlgorithm.java
+++ src/algorithm/StringAlgorithm.java
@@ -265,7 +265,7 @@ public class StringAlgorithm {
                 return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             index++;
         }
-        return negative ? -(int) result : (int)result;
+        return negative ? -(int) result : (int) result;
     }
 
     /**
@@ -276,43 +276,43 @@ public class StringAlgorithm {
      * @return
      */
     public String reverseWords(String s) {
-        if(s == null || s.trim().length() == 0)
+        if (s == null || s.trim().length() == 0)
             return "";
         s = s.trim();
         List<String> wordList = Arrays.asList(s.split("\\s+"));
         Collections.reverse(wordList);
-        return String.join(" ",wordList);
+        return String.join(" ", wordList);
     }
 
     public String reverseWords_2(String s) {
-        if(s == null || s.trim().length() == 0)
+        if (s == null || s.trim().length() == 0)
             return "";
         s = s.trim();
         StringBuilder stringBuilder = new StringBuilder();
         Deque<String> d = new ArrayDeque<>();
         int left = 0, right = s.length() - 1;
-        while (left <= right){
+        while (left <= right) {
             char c = s.charAt(left);
-            if((stringBuilder.length() != 0) && (c == ' ')){
+            if ((stringBuilder.length() != 0) && (c == ' ')) {
                 d.offerFirst(stringBuilder.toString());
                 stringBuilder.setLength(0);
-            }else if(c!= ' '){
+            } else if (c != ' ') {
                 stringBuilder.append(c);
             }
             ++left;
         }
         d.offerFirst(stringBuilder.toString());
-        return String.join(" ",d);
+        return String.join(" ", d);
     }
 
     /**
      * 466. 统计重复个数
      * 由 n 个连接的字符串 s 组成字符串 S，记作 S = [s,n]。例如，["abc",3]=“abcabcabc”。
-     *
+     * <p>
      * 如果我们可以从 s2 中删除某些字符使其变为 s1，则称字符串 s1 可以从字符串 s2 获得。例如，根据定义，"abc" 可以从 “abdbec” 获得，但不能从 “acbbe” 获得。
-     *
+     * <p>
      * 现在给你两个非空字符串 s1 和 s2（每个最多 100 个字符长）和两个整数 0 ≤ n1 ≤ 106 和 1 ≤ n2 ≤ 106。现在考虑字符串 S1 和 S2，其中 S1=[s1,n1] 、S2=[s2,n2] 。
-     *
+     * <p>
      * 请你找出一个可以满足使[S2,M] 从 S1 获得的最大整数 M 。
      *
      * @param s1
@@ -322,7 +322,7 @@ public class StringAlgorithm {
      * @return
      */
     public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
-        if(n1 == 0)
+        if (n1 == 0)
             return 0;
         char[] c1 = s1.toCharArray();
         char[] c2 = s2.toCharArray();
@@ -330,30 +330,30 @@ public class StringAlgorithm {
         int l2 = s2.length();
         int countS1 = 0; // 经历了多少个s1
         int countS2 = 0; // 经历了多少个s2
-        int p=0;         // 当前在s2的位置
-        Map<Integer,int[]> mp = new HashMap<>(); //记录每一次s1 扫描结束后当前的状态，寻找循环
-        while (countS1 < n1){
-            for(int i = 0; i < l1;i++){
-                if(c1[i] == c2[p]){
+        int p = 0;         // 当前在s2的位置
+        Map<Integer, int[]> mp = new HashMap<>(); //记录每一次s1 扫描结束后当前的状态，寻找循环
+        while (countS1 < n1) {
+            for (int i = 0; i < l1; i++) {
+                if (c1[i] == c2[p]) {
                     p++;
-                    if(p==l2){
-                        p =0 ;
+                    if (p == l2) {
+                        p = 0;
                         countS2++;
                     }
                 }
             }
             countS1++;
-            if(!mp.containsKey(p)){
-                mp.put(p,new int[]{countS1,countS2}); //记录当前状态
-            }else{
+            if (!mp.containsKey(p)) {
+                mp.put(p, new int[]{countS1, countS2}); //记录当前状态
+            } else {
                 int[] last = mp.get(p);
                 int circle1 = countS1 - last[0];
                 int circle2 = countS2 - last[1];
-                countS2 += circle2 * ((n1-countS1)/circle1);
-                countS1 = countS1 + ((n1-countS1)/circle1)*circle1;
+                countS2 += circle2 * ((n1 - countS1) / circle1);
+                countS1 = countS1 + ((n1 - countS1) / circle1) * circle1;
             }
         }
-        return countS2/n2;
+        return countS2 / n2;
     }
 
     /**
@@ -365,43 +365,44 @@ public class StringAlgorithm {
      * @return
      */
     public String reverseLeftWords(String s, int n) {
-        if(s == null)
+        if (s == null)
             return null;
-        if(n > s.length())
+        if (n > s.length())
             return s;
-        return s.substring(n) + s.substring(0,n);
+        return s.substring(n) + s.substring(0, n);
     }
 
     /**
      * 680. 验证回文字符串 Ⅱ
-     *
+     * <p>
      * 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+     *
      * @param s
      * @return
      */
     public boolean validPalindrome(String s) {
-        if(s == null || s.length() == 0)
+        if (s == null || s.length() == 0)
             return false;
-        if(s.length() <= 2)
+        if (s.length() <= 2)
             return true;
-        int left = 0,right = s.length() - 1;
-        while (left < right){
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
             char c1 = s.charAt(left), c2 = s.charAt(right);
-            if(c1 == c2){
+            if (c1 == c2) {
                 left++;
                 right--;
-            }else{
-                boolean flag1 = true,flag2 = true;
-                for(int i = left,j = right - 1;i < j; i++,j--){
-                    char c3 = s.charAt(i),c4 = s.charAt(j);
-                    if(c3 != c4){
+            } else {
+                boolean flag1 = true, flag2 = true;
+                for (int i = left, j = right - 1; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
                         flag1 = false;
                         break;
                     }
                 }
-                for(int i = left + 1,j = right;i<j;i++,j--){
-                    char c3 = s.charAt(i),c4 = s.charAt(j);
-                    if(c3 != c4){
+                for (int i = left + 1, j = right; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
                         flag2 = false;
                         break;
                     }
@@ -414,39 +415,70 @@ public class StringAlgorithm {
 
     /**
      * 1371. 每个元音包含偶数次的最长子字符串
-     *
+     * <p>
      * 给你一个字符串 s ，请你返回满足以下条件的最长子字符串的长度：每个元音字母，即 'a'，'e'，'i'，'o'，'u' ，在子字符串中都恰好出现了偶数次。
      *
      * @param s
      * @return
      */
     public int findTheLongestSubstring(String s) {
-        if(s == null || s.length() == 0)
+        if (s == null || s.length() == 0)
             return 0;
         int n = s.length();
         int[] pos = new int[1 << 5];
-        Arrays.fill(pos,-1);
+        Arrays.fill(pos, -1);
         int ans = 0, status = 0;
         pos[0] = 0;
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if(ch == 'a'){
+            if (ch == 'a') {
                 status ^= (1 << 0);
-            }else if(ch == 'e'){
+            } else if (ch == 'e') {
                 status ^= (1 << 1);
-            }else if(ch== 'i'){
+            } else if (ch == 'i') {
                 status ^= (1 << 2);
-            }else if(ch == 'o'){
-                status ^= (1<<3);
-            }else if(ch == 'u'){
-                status ^= (1<<4);
+            } else if (ch == 'o') {
+                status ^= (1 << 3);
+            } else if (ch == 'u') {
+                status ^= (1 << 4);
             }
-            if(pos[status] >= 0){
-                ans = Math.max(ans,i + 1 - pos[status]);
-            }else{
+            if (pos[status] >= 0) {
+                ans = Math.max(ans, i + 1 - pos[status]);
+            } else {
                 pos[status] = i + 1;
             }
         }
         return ans;
+    }
+
+    /**
+     * 5. 最长回文子串
+     * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for(int i = 0; i < s.length(); i++){
+            int len1 = expandAroundCenter(s,i,i);
+            int len2 = expandAroundCenter(s,i,i+1);
+            int len = Math.max(len1,len2);
+            if(len > end - start){
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
     }
 }
