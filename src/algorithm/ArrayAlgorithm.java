4da2721afee680650e33f8590c64bcdf9eb9b7a5
Skywing <wangty9153@126.com>
2020-06-19 13:48:50 +0800

    1470. 重新排列数组

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -1996,4 +1996,27 @@ public class ArrayAlgorithm {
         return runningSum;
     }
 
+    /**
+     * 1470. 重新排列数组
+     *
+     * 给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+     *
+     * 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+     *
+     * @param nums
+     * @param n
+     * @return
+     */
+    public int[] shuffle(int[] nums, int n) {
+        if(nums == null || nums.length == 0)
+            return new int[0];
+        if(nums.length < 2 * n)
+            n = nums.length / 2;
+        int[] result = new int[2 * n];
+        for(int i = 0; i < n; i++){
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
+        }
+        return result;
+    }
 }
