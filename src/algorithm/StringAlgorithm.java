c89ff3d79ceca2f9e4b62afc6495002c3d593df3
Skywing <wangty9153@126.com>
2020-08-26 14:12:25 +0800

    17. 电话号码的字母组合

--- src/algorithm/StringAlgorithm.java
+++ src/algorithm/StringAlgorithm.java
@@ -1487,4 +1487,48 @@ public class StringAlgorithm {
         }
         return false;
     }
+
+    /**
+     * 17. 电话号码的字母组合
+     *
+     * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+     *
+     * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+     *
+     * @param digits
+     * @return
+     */
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if(digits.length() == 0){
+            return combinations;
+        }
+        Map<Character,String> phoneMap = new HashMap<Character, String>(){{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrackLetterCombinations(combinations,phoneMap,digits,0,new StringBuffer());
+        return combinations;
+    }
+
+    private void backtrackLetterCombinations(List<String> combinations, Map<Character,String> phoneMap, String digits,int index,StringBuffer combination){
+        if(index == digits.length()){
+            combinations.add(combination.toString());
+        }else{
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for(int i = 0; i < lettersCount; i++){
+                combination.append(letters.charAt(i));
+                backtrackLetterCombinations(combinations,phoneMap,digits,index + 1,combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
 }
