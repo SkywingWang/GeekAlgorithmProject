e5b786917802f5bf0bb2f636968c2f5147f40f05
Skywing <wangty9153@126.com>
2021-04-28 14:06:12 +0800

    平方数之和

--- src/algorithm/ArrayAlgorithm.java
+++ src/algorithm/ArrayAlgorithm.java
@@ -4852,4 +4852,27 @@ public class ArrayAlgorithm {
         }
         return dp[target];
     }
+
+    /**
+     * 938. 二叉搜索树的范围和
+     *
+     * 给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+     *
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null){
+            return 0;
+        }
+        if(root.val > high){
+            return rangeSumBST(root.left,low,high);
+        }
+        if(root.val < low){
+            return rangeSumBST(root.right,low,high);
+        }
+        return root.val + rangeSumBST(root.left,low,high) + rangeSumBST(root.right,low,high);
+    }
 }
